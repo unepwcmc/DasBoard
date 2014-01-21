dc7e1eeb7143b341d09b03fe49d60cc008512c19
James Cox <th3james@gmail.com>
2014-01-21 13:51:55 +0000

    It's a rails app now!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DasBoard::Application.config.secret_key_base = 'c1e5dbed66bf9d27123a184ed661ca186a4e4e14cbf6d4bd02b6e899d6fcd227b72883fefa261cbb0a585558ec6669d95d8ed6703a699ecf8369c59839088422'
