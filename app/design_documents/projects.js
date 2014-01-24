757c77cc11491e5ef11383f67b31533b048161de
James Cox <th3james@gmail.com>
2014-01-24 17:17:34 +0000

    Add projects/with_nested_objectives view

--- app/design_documents/projects.js
+++ app/design_documents/projects.js
@@ -9,6 +9,32 @@
           emit([doc.project_id, 1, doc.type], doc);
         }
       }
+    }, 
+    with_nested_objectives: {
+      map: function(doc) {
+        if (doc.type == 'project') {
+          emit([doc._id, 0, doc.type], doc);
+        } else if (doc.type == 'objective') {
+          emit([doc.project_id, 1, doc.type], doc);
+        }
+      },
+      reduce: function(keys, values) {
+        var parentProject = null;
+        var childObjectives = [];
+        for (var i=0, il=values.length; i<il; i++) {
+          var value = values[i];
+          var type = keys[i][0][1];
+          if (type === 0) {
+            parentProject = value;
+          } else {
+            childObjectives.push(value);
+          }
+        }
+
+        parentProject.objectives = childObjectives;
+
+        return parentProject;
+      }
     }
   }
 };
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -16,24 +16,54 @@ class ProjectTest < ActiveSupport::TestCase
   end
 
   test 'projects/with_nested_objectives returns the project objectives' do
-    project_with_2_objectives = Couch::Db.post({type: "project"})
-    project_with_1_objective = Couch::Db.post({type: "project"})
+    nrt_project = Couch::Db.post({name: "NRT", type: "project"})
+    species_project = Couch::Db.post({name: "Species+", type: "project"})
 
-    Couch::Db.post({
-      project_id: project_with_2_objectives['id'],type: "objective"
+    first_child_of_nrt = Couch::Db.post({
+      project_id: nrt_project['id'],type: "objective", name: "objective 1 tim"
     })
-    Couch::Db.post({
-      project_id: project_with_2_objectives['id'],type: "objective"
+    second_child_of_nrt = Couch::Db.post({
+      project_id: nrt_project['id'],type: "objective", name: "objective 2 adam"
     })
-    Couch::Db.post({
-      project_id: project_with_1_objective['id'],type: "objective"
+    child_of_species = Couch::Db.post({
+      project_id: species_project['id'],type: "objective", name: "objective 3 james"
     })
 
-    results = Couch::Db.get('_design/projects/_view/with_nested_objectives')
+    results = Couch::Db.get('_design/projects/_view/with_nested_objectives?group=true&group_level=1')
 
-    assert_equal 2, results['rows']
+    assert_equal 2, results['rows'].length
 
-    assert_equal 2, results['rows'][0]['value']
-    assert_equal 1, results['rows'][1]['value']
+    first_result = results['rows'][0]['value']
+    assert_equal nrt_project['id'], first_result['_id'],
+      "Expected the first returned project to be NRT"
+
+    assert_kind_of Array, first_result['objectives'],
+      "Expected the first result to have an array of nested objectives"
+    nested_objectives = first_result['objectives']
+    assert_equal 2, nested_objectives.length,
+      "Expected the first project to have 2 nested objectives"
+
+    nested_objective_ids = nested_objectives.map{|objective| objective['_id']}
+
+    assert nested_objective_ids.include?(first_child_of_nrt['id']),
+      "Expected NRT to have the first child nested"
+    assert nested_objective_ids.include?(second_child_of_nrt['id']),
+      "Expected NRT to have the second child nested"
+
+
+    second_result = results['rows'][1]['value']
+    assert_equal species_project['id'], second_result['_id'],
+      "Expected the second returned project to be species"
+
+    assert_kind_of Array, second_result['objectives'],
+      "Expected species to have an array of nested objectives"
+    nested_objectives = second_result['objectives']
+    assert_equal 1, nested_objectives.length,
+      "Expected species to have 1 nested objective"
+
+    nested_objective_ids = nested_objectives.map{|objective| objective['_id']}
+
+    assert nested_objective_ids.include?(child_of_species['id']),
+      "Expected species to have the correct nested objective"
   end
 end
