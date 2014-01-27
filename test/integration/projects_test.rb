24d6c56a8723b1fff42f3dd23c4fdf312225240a
Adam Mulligan <adammulligan@gmail.com>
2014-01-27 13:57:38 +0000

    Not rendering objectives on the project list as there's no use case for that

--- test/integration/projects_test.rb
+++ test/integration/projects_test.rb
@@ -25,40 +25,4 @@ class ProjectsTest < ActionDispatch::IntegrationTest
       }, 'Expected to see project name'
     end
   end
-
-  test "/projects renders the list of objectives for each project" do
-    project = {
-      id: 'abc',
-      name: 'An project',
-      objectives: [{
-        "_id" => '1',
-        "name" => 'objective 1'
-      },{
-        "_id" => '2',
-        "name" => 'objective 2'
-      }]
-    }.stringify_keys
-
-    Couch::Db.
-      expects(:get).
-      with('_design/projects/_view/with_nested_objectives').
-      returns({'rows' => [{
-        "value" => project
-      }]})
-
-    get '/projects'
-
-    assert_response :success
-
-    assert_select "ul#projects" do
-      assert_select "> li", {
-        count: 1,
-      }, 'Expected there only to be one project'
-    end
-
-    assert_select "ul#projects ul" do
-      assert_select "li:first-child", { count: 1, text: "objective 1" }
-      assert_select "li:last-child", { count: 1, text: "objective 2" }
-    end
-  end
 end
