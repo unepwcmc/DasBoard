ba2d0ccf8cbc8178b74a83b703952d218e2f0ae5
Adam Mulligan <adammulligan@gmail.com>
2014-02-20 11:44:11 +0000

    Remove references to couchdb adapter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,11 +5,6 @@ require 'rails/test_help'
 require "minitest/autorun"
 
 class ActiveSupport::TestCase
-  def setup
-    Couch::Db.delete_database
-    Couch::Db.create_database
-    Couch.load_design_documents
-  end
 end
 
 require 'mocha/test_unit'
