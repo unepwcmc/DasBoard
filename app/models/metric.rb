3798cb07c502facf391a3255f508e7eaf186aa28
Adam Mulligan <adammulligan@gmail.com>
2014-02-20 15:37:20 +0000

    Clone JSON values from database before editing, otherwise ActiveRecord will assume no changes have been made

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,11 +1,10 @@
 class Metric < ActiveRecord::Base
   def add_data_point data
-    puts "changing data to : #{data}"
     self.data ||= []
-    puts "Data object id am: #{self.data.object_id}"
+
     cloned_data = self.data.clone
-    puts "cloned object id am: #{cloned_data.object_id}"
+    cloned_data.push data
+
     self.data = cloned_data
-    puts "Changes to data: #{self.changes}"
   end
 end
